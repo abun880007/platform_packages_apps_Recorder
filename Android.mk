9d93ef8c8e95a5df73451a34f4799283d5887ee5
Harsh Shandilya <harsh@prjkt.io>
2019-09-02 16:00:51 +0530

    Move Android Makefile to root of repo
    Change-Id: I6a9d61c4c3214089453bb95986aa3fb72867d108
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>
    
    # Conflicts:
    #	Android.mk

--- Android.mk
+++ Android.mk
@@ -5,7 +5,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#	  http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,10 +17,12 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/app/src/main/res
+
+LOCAL_MANIFEST_FILE := app/src/main/AndroidManifest.xml
 
 LOCAL_SRC_FILES := \
-    $(call all-java-files-under, java)
+	$(call all-java-files-under, app/src/main/java)
 
 
 LOCAL_USE_AAPT2 := true
@@ -28,14 +30,14 @@ LOCAL_USE_AAPT2 := true
 LOCAL_PROGUARD_ENABLED := disabled
 
 LOCAL_STATIC_ANDROID_LIBRARIES += \
-    androidx.annotation_annotation \
-    androidx-constraintlayout_constraintlayout \
-    androidx-constraintlayout_constraintlayout-solver \
-    androidx.appcompat_appcompat \
-    androidx.cardview_cardview \
-    androidx.recyclerview_recyclerview \
-    com.google.android.material_material \
-    androidx.preference_pixel
+	androidx.annotation_annotation \
+	androidx-constraintlayout_constraintlayout \
+	androidx-constraintlayout_constraintlayout-solver \
+	androidx.appcompat_appcompat \
+	androidx.cardview_cardview \
+	androidx.recyclerview_recyclerview \
+	com.google.android.material_material \
+	androidx.preference_pixel
 
 LOCAL_PACKAGE_NAME := Recorder
 LOCAL_CERTIFICATE := platform
